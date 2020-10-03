f58f85a90ad268a78dbd0e443e1bef09513885ad
panda5721 <72316284+panda5721@users.noreply.github.com>
2020-10-03 23:40:43 +0530

    Update addTwoNumbers.java

--- addTwoNumbers.java
+++ addTwoNumbers.java
@@ -1,3 +1,7 @@
-int a = 5;
-int b = 10;
-int add = a+b;
+public class addtw{
+  public static void main(String args[]){
+    int a = 5;
+    int b = 6;
+    System.out.println("the sum is "+ a+b);
+  }
+}
